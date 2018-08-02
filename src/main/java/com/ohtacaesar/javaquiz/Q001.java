6cbdb2cfdce2d77ad694b7d4f45cfadbb59ddfe8
ohtacaesar <ohtacaesar@gmail.com>
2018-08-02 22:52:13 +0900

    Q001 String.equels

--- src/main/java/com/ohtacaesar/javaquiz/Q001.java
+++ src/main/java/com/ohtacaesar/javaquiz/Q001.java
@@ -1,5 +1,12 @@
 package com.ohtacaesar.javaquiz;
 
-public class Q001 {
+class Q001 {
+
+  /**
+   * String a と b が同じ文章の場合はtrue, 異なる文章の場合はfalseを返すメソッドを実装してください。
+   */
+  boolean run(String a, String b) {
+    return true;
+  }
 
 }
--- src/test/java/com/ohtacaesar/javaquiz/Q001Test.java
+++ src/test/java/com/ohtacaesar/javaquiz/Q001Test.java
@@ -1,13 +1,26 @@
 package com.ohtacaesar.javaquiz;
 
-import org.junit.jupiter.api.Assertions;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import org.junit.jupiter.api.Test;
 
 class Q001Test {
 
+  private Q001 q = new Q001();
+
   @Test
   void test01() {
-    Assertions.assertTrue(true);
+    assertFalse(q.run("test1", "test2"));
   }
 
+  @Test
+  void test02() {
+    assertTrue(q.run("test3", "test3"));
+  }
+
+  @Test
+  void test03() {
+    assertTrue(q.run(new String("test4"), new String("test4")));
+  }
 }
